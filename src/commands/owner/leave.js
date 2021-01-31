bdc7cdbb4407dffa3e1c7afb4906bc4920194503
Lucius <72539206+Adoniswoah@users.noreply.github.com>
2021-01-31 16:55:57 +0530

    Create leave.js

---
+++ src/commands/owner/leave.js
@@ -0,0 +1,13 @@
+exports.conf = {
+  enabled: true,
+  aliases: ['p'],
+  guildOnly: false,
+  permLevel: 'Bot Owner'
+}
+
+/** Command Help */
+exports.help = {
+  name: 'leave',
+  usage: '',
+  description: 'Leave the guild I\'m currently in.'
+}
