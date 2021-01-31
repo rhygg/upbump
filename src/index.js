80288a84c0e1a8e76c3a2541af9fb1c6082cd208
Lucius <72539206+Adoniswoah@users.noreply.github.com>
2021-01-31 16:43:01 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,40 @@
+if (Number(process.version.slice(1).split('.')[0]) < 8) throw new Error('Node 8.0.0 or higher is required. Update Node on your system.')
+
+const keepAlive = require('./server');
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.database = require('sqlite')
+client.config = require('./config.js')
+client.loader = require('./modules/Loader')
+
+client.commands = new Discord.Collection()
+client.aliases = new Discord.Collection()
+
+require('dotenv').config()
+require('./modules/functions.js')(client)
+
+client.database.open('./database.sqlite') // Create database
+
+// Cache
+client.levelCache = {}
+for (const permLevel of client.config.permLevels) {
+  client.levelCache[permLevel.name] = permLevel.level
+}
+
+const init = async () => {
+  console.clear()
+
+  console.log('👌 Initiated!')
+
+  const loader = await client.loader
+  await loader.registerModules(client)
+  await loader.registerCommands(client)
+  await loader.registerEvents(client)
+  await loader.checkDiscordStatus(client)
+
+  keepAlive();
+  await client.login(process.env.BOT_TOKEN)
+}
+
+init()
